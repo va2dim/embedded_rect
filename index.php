9da8da7abd4dbf5bee3b350e015e5838c81d8a39
va2dim <va2dim@gmail.com>
2021-01-18 00:54:17 +0200

    Added calculation & checking limitation

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+/**
+ * Calculate for rectangle amount of all embedded rectangles with intersection
+ * @param int $m > 1
+ * @param int $n > 1
+ * @return int (= 0 for incorrect args)
+ */
+function countEmbeddedRect(int $m, int $n) {
+    if (($m <= 1) || ($n <= 1)) {
+        var_dump ('Incorrect amount of dots, must be more than 1 for any side!');
+        return 0;
+    }
+
+    return $m * ($m - 1) * $n * ($n - 1) / 4 ;
+}
+
+var_dump (countEmbeddedRect(9,9));
